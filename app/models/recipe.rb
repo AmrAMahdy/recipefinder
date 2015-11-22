bdcff737af87f904a0dcabf18f334d989355bd60
Amr Alaa <n1amr1@gmail.com>
2015-11-22 02:27:44 +0200

    update GemFile

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,13 @@
 class Recipe
   include HTTParty
   
-  base_uri "http://food2fork.com/api"
   default_params key: "40725c83ff55a71d2546e9b12749384e" 
   format :json
   
   key_value = ENV['FOOD2FORK_KEY']
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
-  
+  base_uri "http://#{ hostport }/api"
+
   def self.for term
     get("/search", query: {q: term})["recipes"]
   end
